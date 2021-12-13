8f9f16adf5a9d166cec7621e909dbc0bb02fe685
ИСП-34 <-34@LAB30-13>
2021-12-13 11:18:49 +0300

    Добавьте файлы проекта.

---
+++ Lib_13/Practice.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib_13
+{
+    public class Practice
+    {
+        /// <summary>
+        /// Генерировать случайные числа Х, распределенные в диапазоне от -5 до 5 и
+        /// и вычислять для чисел > 0 корень из X, а для чисел < 0 Х в степени 2
+        /// </summary>
+        /// <returns>Вычисление корня из Х или возведение во Х во 2 степень и вывод сгенерированные чисел</returns>
+        public static (string allNumbers, string calculations) GetSqrtOrSquare()
+        {
+            Random random = new Random();
+
+            string numbers = string.Empty;
+
+            string calculation = string.Empty;
+
+            int temporaryValue = -6;
+            int currentValue = random.Next(-5, 5);
+
+            do
+            {
+                numbers += currentValue.ToString() + ", ";
+
+                if (currentValue > 0)
+                {
+                    calculation += Math.Sqrt(currentValue).ToString() + ", ";
+                }
+                else 
+                {
+                    calculation += (currentValue * currentValue).ToString() + ", ";
+                }
+                temporaryValue = currentValue;
+                currentValue = random.Next(-5, 5);
+
+            } while (currentValue != temporaryValue);
+            numbers = numbers.Remove(numbers.Length - 2) + ".";
+
+            calculation = calculation.Remove(calculation.Length - 2) + ".";
+
+            var result = (numbers, calculation);
+
+            return result;
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Lib_13;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GetSqrtOrSquare_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var result = Practice.GetSqrtOrSquare();
+
+                AllNumbers.Text = result.allNumbers;
+
+                Result.Text = result.calculations;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ошибка!");
+            }
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Information_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Золотарев М.А ИСП-34", "Разработчик", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
